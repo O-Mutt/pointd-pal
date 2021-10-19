d775e405ba366e92f79faedaeb9509ebd3caee0e
Matthew O'Keefe <matt@okeefe.dev>
2021-10-19 06:12:01 -0500

    duh, copy pasta failure

--- src/lib/services/connectionsFactory.ts
+++ src/lib/services/connectionsFactory.ts
@@ -7,8 +7,5 @@ const procVars = Helpers.getProcessVariables(process.env);
 export function connectionFactory(teamId: string) {
   const connectionUri = procVars.mongoUri.replace('#TEAM_ID', teamId);
   const conn = mongoose.createConnection(`${connectionUri}`);
-  
-  conn.model('User', require('../schemas/user'));
-  conn.model('PageView', require('../schemas/pageView'));
   return conn; 
 };
