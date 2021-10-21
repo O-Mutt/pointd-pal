6beb2af49c594f7a2d508cd5fbe36f555d043661
Matthew O'Keefe <matt@okeefe.dev>
2021-10-21 11:12:17 -0500

    Store the connections for later use

--- src/lib/services/connectionsFactory.ts
+++ src/lib/services/connectionsFactory.ts
@@ -4,9 +4,13 @@ import mongoose from 'mongoose';
 
 const procVars = Helpers.getProcessVariables(process.env);
 
+const connections: { [key: string]: mongoose.Connection } = {};
 export function connectionFactory(teamId?: string): mongoose.Connection {
-  const connectionUri = procVars.mongoUri.replace('#TEAM_ID', teamId || procVars.defaultDb);
-  console.log(procVars.mongoUri);
-  const conn = mongoose.createConnection(`${connectionUri}`);
-  return conn;
+  const databaseName = teamId || procVars.defaultDb || '';
+  if (connections[databaseName]) {
+    return connections[databaseName];
+  }
+  const connectionUri = procVars.mongoUri.replace('#TEAM_ID', teamId || procVars.defaultDb || '');
+  connections[databaseName] = mongoose.createConnection(`${connectionUri}`);
+  return connections[databaseName];
 }
