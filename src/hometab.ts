b862a8cce8ee2e8cf41615fa342ad286e07ec808
Matthew O'Keefe <matt@okeefe.dev>
2021-10-29 12:26:58 -0500

    Try some more bits and bobs for getting the correct contextual token for the api

--- src/hometab.ts
+++ src/hometab.ts
@@ -36,7 +36,8 @@ async function updateHomeTab(args: SlackEventMiddlewareArgs<'app_home_opened'> &
 
       //...getBonuslyAdminConfigSection(user, bonusly, qraftyConfig),
     );
-    const result = await args.client.views.publish({ token: args.context.botToken, view: hometab.buildToObject() as View, user_id: userId });
+    args.logger.debug(args.context, args.payload, args.body, args.event);
+    const result = await args.client.views.publish({ token: args.body.token, view: hometab.buildToObject() as View, user_id: userId });
   } catch (e) {
     args.logger.error('error publishing hometab', e);
   }
