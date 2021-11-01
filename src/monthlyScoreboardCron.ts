ffd5125d30ee00a1257d8a8b72b6c36fc86ad2c6
Matthew O'Keefe <matt@okeefe.dev>
2021-11-01 12:45:01 -0500

    Shrinking the scoreboard to one message

--- src/monthlyScoreboardCron.ts
+++ src/monthlyScoreboardCron.ts
@@ -4,7 +4,7 @@ import { CronJob } from 'cron';
 import ImageCharts from 'image-charts';
 import _ from 'lodash';
 import moment from 'moment';
-import { Blocks, Md, Message } from 'slack-block-builder';
+import { Appendable, BlockBuilder, Blocks, Md, Message } from 'slack-block-builder';
 
 import { app } from '../app';
 import { Helpers as H } from './lib/helpers';
@@ -54,38 +54,26 @@ const databaseService = new DatabaseService({ ...procVars });
             messages.push(`${rank}. ${Md.user(sender._id)} (${sender.scoreChange} ${pointStr})`);
             rank++;
           }
-
-          const topSenderMessage = buildChartMessage(channelId, `Qrafty 10 Qrafty Point Senders over the last month`, topSenders, messages);
-          try {
-            const result = await app.client.chat.postMessage({ token: botToken, ...topSenderMessage } as ChatPostMessageArguments);
-          } catch (e: any) {
-            console.error('error', e.data.response_metadata.message);
-          }
-
+          const topSenderBlocks = buildBlocks(`Top 10 Qrafty Point Senders`, topSenders, messages);
 
           // Recipients
           const topRecipients = await databaseService.getTopReceiverInDuration(connection, 10, 30);
           messages = [];
           rank = 1;
           for (const recipient of topRecipients) {
-            const pointStr = `point${H.getEsOnEndOfWord(recipient.scoreChange)} given`;
+            const pointStr = `point${H.getEsOnEndOfWord(recipient.scoreChange)} received`;
             console.log(`Top room [i] ${JSON.stringify(topRecipients)}[${rank}]`);
             messages.push(`${rank}. ${Md.user(recipient._id)} (${recipient.scoreChange} ${pointStr})`);
             rank++;
           }
-
-          const topRecipientMessage = buildChartMessage(channelId, `Top 10 Qrafty Point Recipients over the last month`, topRecipients, messages);
-          try {
-            const result = await app.client.chat.postMessage({ token: botToken, ...topRecipientMessage } as ChatPostMessageArguments);
-          } catch (e: any) {
-            console.error('error', e.data.response_metadata.message);
-          }
+          const topRecipientBlocks = buildBlocks(`Top 10 Qrafty Point Recipients`, topRecipients, messages);
 
           // Channel
           let topRooms = await databaseService.getTopRoomInDuration(connection, 3, 30);
           messages = [];
           rank = 1;
           for (const room of topRooms) {
+            console.log('find the room', room);
             const { channel } = await app.client.conversations.info({ token: botToken, channel: room._id });
             if (channel) {
               room.name = channel.name;
@@ -96,9 +84,22 @@ const databaseService = new DatabaseService({ ...procVars });
             rank++;
           }
 
-          const topRoomMessage = buildChartMessage(channelId, `Top 3 Channels that sent the most Qrafty Point over the last month`, topRooms, messages, true);
+          const topRoomBlocks = buildBlocks(`Top 3 Qrafty Point Channels`, topRooms, messages);
+          const theMessage = Message({ channel: channelId, text: 'Monthly Qrafty Scoreboard' })
+            .blocks(
+              Blocks.Header({ text: 'Monthly Qrafty Scoreboard' }),
+              Blocks.Divider(),
+              ...topSenderBlocks,
+              Blocks.Divider(),
+              ...topRecipientBlocks,
+              Blocks.Divider(),
+              ...topRoomBlocks,
+            )
+            .asUser()
+            .buildToObject();
+
           try {
-            const result = await app.client.chat.postMessage({ token: botToken, ...topRoomMessage } as ChatPostMessageArguments);
+            const result = await app.client.chat.postMessage({ token: botToken, ...theMessage } as ChatPostMessageArguments);
           } catch (e: any) {
             console.error('error', e.data.response_metadata.message);
           }
@@ -113,25 +114,21 @@ const databaseService = new DatabaseService({ ...procVars });
   }
 })();
 
-function buildChartMessage(channel: string, title: string, tops: any[], messages: string[], isChannel: boolean = false) {
-  const chartText = title;
+function buildBlocks(title: string, tops: any[], messages: string[]) {
   const graphSize = Math.min(tops.length, Math.min(10, 20));
   const chartUrl = new ImageCharts()
     .cht('bvg')
     .chs('999x200')
-    .chtt(chartText)
+    .chtt(title)
     .chxt('x,y')
     .chxl(`0:|${_.take(_.map(tops, 'name'), graphSize).join('|')}`)
     .chd(`a:${_.take(_.map(tops, 'scoreChange'), graphSize).join(',')}`)
     .toURL();
 
-  const theMessage = Message({ channel: channel, text: chartText })
-    .blocks(
-      Blocks.Header({ text: chartText }),
-      Blocks.Image({ imageUrl: chartUrl, altText: chartText }),
-      Blocks.Section({ text: messages.join('\n') }),
-    )
-    .asUser()
-    .buildToObject();
-  return theMessage;
+  let blocks: Appendable<BlockBuilder> = [
+    Blocks.Section({ text: Md.bold(Md.italic(title)) }),
+    Blocks.Image({ imageUrl: chartUrl, altText: title }),
+    Blocks.Section({ text: messages.join('\n') })
+  ];
+  return blocks;
 }
