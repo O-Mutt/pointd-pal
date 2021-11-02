d918fc6d8133f06701a9c7e88f9c0c9ab1eb75fe
Matthew O'Keefe <matt@okeefe.dev>
2021-11-02 04:47:07 -0500

    Remove first then create for install store

--- src/lib/services/qraftyInstallStore.ts
+++ src/lib/services/qraftyInstallStore.ts
@@ -1,81 +1,64 @@
-import { InstallationStore, Installation as OAuthInstallation } from "@slack/oauth";
-import { Installation } from "../models/installation"
+import { InstallationStore, Installation as OAuthInstallation, InstallationQuery } from '@slack/oauth';
+import { Installation } from '../models/installation'
 
 
 export const QraftyInstallStore: InstallationStore = {
   storeInstallation: async (installation) => {
-    // change the lines below so they save to your database
+
+    let teamId;
     if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
-      // support for org-wide app installation
-      console.log("[INSTALL] org wide", installation.enterprise.id);
-      await Installation.findOneAndUpdate(
-        {
-          teamId: installation.enterprise.id
-        },
-        {
-          teamId: installation.enterprise.id,
-          installation,
-        },
-        {
-          upsert: true
-        }
-      );
-      return;
+      console.log(`[INSTALL] org wide ${installation.enterprise.id}`);
+      teamId = installation.enterprise.id;
     }
     if (installation.team !== undefined) {
-      // single team app installation
-      console.log("[INSTALL] single team", installation.team.id);
-      await Installation.findOneAndUpdate(
-        {
-          teamId: installation.team.id
-        },
-        {
-          teamId: installation.team.id,
-          installation,
-        },
-        {
-          upsert: true
-        }
-      );
+      console.log(`[INSTALL] single team ${installation.team.id}`);
+      teamId = installation.team.id;
+    }
+
+    if (teamId) {
+      await Installation.remove({ teamId });
+      await Installation.create({
+        teamId,
+        installation,
+      });
       return;
     }
     throw new Error('Failed saving installation data to installationStore');
   },
-  fetchInstallation: async (installQuery) => {
-    // change the lines below so they fetch from your database
+  fetchInstallation: async (installQuery: InstallationQuery<boolean>): Promise<OAuthInstallation<'v1' | 'v2', boolean>> => {
+    let teamId;
     if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-      // org wide app installation lookup
-      console.log("[LOOKUP] org wide app", installQuery.enterpriseId);
-      const result = await Installation.findOne({ teamId: installQuery.enterpriseId }).exec();
-      if (!result) {
-        throw new Error('Failed fetching installation');
-      }
-      return result.installation as OAuthInstallation<"v1" | "v2", boolean>;
+      console.log(`[LOOKUP] org wide app ${installQuery.enterpriseId}`);
+      teamId = installQuery.enterpriseId;
     }
     if (installQuery.teamId !== undefined) {
-      // single team app installation lookup
-      console.log("[LOOKUP] single team app", installQuery.teamId);
-      const result = await Installation.findOne({ teamId: installQuery.teamId }).exec();
+      console.log(`[LOOKUP] single team app ${installQuery.teamId}`);
+      teamId = installQuery.teamId;
+    }
+
+    if (teamId) {
+      const result = await Installation.findOne({ teamId }).exec();
       if (!result) {
         throw new Error('Failed fetching installation');
       }
-      return result.installation as OAuthInstallation<"v1" | "v2", boolean>;
+      console.log(`[LOOKUP]  ${teamId}.`)
+      return result.installation as OAuthInstallation<'v1' | 'v2', boolean>;
     }
-    throw new Error('Failed fetching installation');
+
+    throw new Error('Failed fetching installation, failed overall');
   },
-  deleteInstallation: async (installQuery) => {
-    // change the lines below so they delete from your database
+  deleteInstallation: async (installQuery: InstallationQuery<boolean>): Promise<void> => {
+    let teamId;
     if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-      // org wide app installation deletion
-      await Installation.deleteOne({ teamId: installQuery.enterpriseId }).exec();
-      return;
+      teamId = installQuery.enterpriseId;
     }
     if (installQuery.teamId !== undefined) {
-      // single team app installation deletion
-      await Installation.deleteOne({ teamId: installQuery.teamId }).exec();
-      return;
+      teamId = installQuery.teamId;
     }
-    return;
+
+    if (teamId) {
+      await Installation.deleteOne({ teamId }).exec();
+    }
     throw new Error('Failed to delete installation');
   },
 };
